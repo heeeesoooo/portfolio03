823e893773016262997d2b7796af4da5c95be480
heeeesoooo <118698717+heeeesoooo@users.noreply.github.com>
2022-12-15 17:10:38 +0900

    Add files via upload

--- js/shinsegae.js
+++ js/shinsegae.js
@@ -1,6 +1,6 @@
 $(function () {
-    $('.topBanner i').on('click', function () {
-        $('.topBanner').addClass('on')
+    $('.banner i').on('click', function () {
+        $('.banner').addClass('on')
     })
     $('.textSlide').slick({
         arrows: false,
@@ -15,5 +15,48 @@ $(function () {
         autoplay: true,
         autoplaySpeed: 4000,
     });
+    $('.content_slide').slick({
+        slidesToShow: 3,
+        arrows: false,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 4000,
+    })
+    $('.mainContent .arrows .prev').on('click', function () {
+        $('.content_slide').slick('slickPrev')
+    })
+
+    $('.mainContent .arrows .next').on('click', function () {
+        $('.content_slide').slick('slickNext')
+    })
+
+    $('.main_pro_slide').slick({
+        infinite: true,
+        fade: true,
+        cssEase: 'linear',
+        arrows: false,
+        //dots: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+    });
+
+    $('.tab_list li').on('click', function (e) {
+        e.preventDefault();
+        var idx = $(this).index();
+        $('.main_pro_slide').slick('slickGoTo', idx)
+    })
+
+
+    $(window).on('scroll', function () {
+        console.log('스크롤 : ', $(window).scrollTop());
+        if ($(window).scrollTop() > 0) {
+            $('.header').addClass('on');
+        } else {
+            $('.header').removeClass('on');
+        }
+
+    })
 
 })
+
+
